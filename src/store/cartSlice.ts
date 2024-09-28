e47a1f8bfd5d2322cf69e97fee807449ea50a370
souradipdev <22052939@kiit.ac.in>
2024-09-28 22:22:32 +0530

    Initialised cart slice type

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,2 +1,20 @@
 import {createStore} from "zustand";
 
+import {CartProduct} from "@/types/cartProduct";
+import {Product} from "@/types/product";
+
+interface CartState {
+  products: Array<CartProduct>;
+  total: number;
+}
+
+interface CartActions {
+  addProduct: (product: Product) => void;
+  removeProduct: (productId: String) => void;
+  incrementQty: (productId: String) => void;
+  decrementQty: (productId: String) => void;
+  getProductById: (ProductId: String) => CartProduct | undefined;
+  reset: () => void;
+}
+
+export type CartSlice = CartState & CartActions;
